7f749d201833a95a57a0c5949cc4a0cc15d246f9
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-10 19:44:37 +0500

    add footballer database constants

---
+++ app/src/main/java/xyz/teamgravity/roomsqlcipher/data/local/constant/FootballerConst.kt
@@ -0,0 +1,19 @@
+package xyz.teamgravity.roomsqlcipher.data.local.constant
+
+object FootballerConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "footballer_database"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table footballers
+     */
+    const val TABLE_FOOTBALLER = "table_footballer"
+}
